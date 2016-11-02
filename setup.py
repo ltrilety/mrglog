36f8b92c89ae3cfe0acc5ebb0ad86fdca5e2c17b
Martin Bukatovic <mbukatov@redhat.com>
2016-11-02 12:00:44 +0100

    setuptools configuration (setup.py)

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='mrglog',
+    version='0.1.0dev1',
+    author='Luboš Tříletý',
+    author_email='ltrilety@redhat.com',
+    license='Apache 2.0',
+    url='https://github.com/ltrilety/mrglog',
+    description='MRG log module',
+    long_description=read('README.rst'),
+    py_modules=['mrglog'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: System :: Logging',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    scripts=['mrglog_demo.py'],
+    )
